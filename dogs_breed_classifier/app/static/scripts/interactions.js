3a268889842c8b0c70f72635a9a89e29f22f488a
oamadeus <aoleszczak@gmail.com>
2020-04-22 20:44:17 +0200

    Fix dissapearing image issue

--- dogs_breed_classifier/app/static/scripts/interactions.js
+++ dogs_breed_classifier/app/static/scripts/interactions.js
@@ -3,7 +3,7 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#dog').attr('src', e.target.result);
+            $('#blah').attr('src', e.target.result);
         };
 
         reader.readAsDataURL(input.files[0]);
