3975d13de527a874c4e578d54921121dca96084e
oamadeus <aoleszczak@gmail.com>
2020-04-24 15:41:19 +0200

    Fix issue with loading model weights in code

--- dogs_breed_classifier/model/resnet_model.py
+++ dogs_breed_classifier/model/resnet_model.py
@@ -18,24 +18,18 @@ def create(num_of_outputs):
     Returns:
         ResNet50 model with additional layers at the end
     """
-    global_average_layer = tf.keras.layers.GlobalAveragePooling2D(
-        input_shape=(7, 7, 2048),
-        )
-    dense = tf.keras.layers.Dense(512)
-    dropout = tf.keras.layers.Dropout(0.4)
-    prediction_layer = tf.keras.layers.Dense(num_of_outputs, activation="softmax")
-
     resnet_model = tf.keras.Sequential([
-        global_average_layer,
-        dense,
-        dropout,
-        prediction_layer,
+        tf.keras.layers.GlobalAveragePooling2D(input_shape=(7, 7, 2048)),
+        tf.keras.layers.Dense(1024, activation="selu"),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Dense(num_of_outputs, activation="softmax"),
     ])
 
     resnet_model.compile(
-        loss='categorical_crossentropy',
-        optimizer='adam',
-        metrics=['accuracy']
-        )
+        optimizer="sgd",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
 
     return resnet_model
